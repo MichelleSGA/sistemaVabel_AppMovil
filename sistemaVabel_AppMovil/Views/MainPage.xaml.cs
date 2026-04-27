1f6a5a452bd51aba21d31bd68dd9dd13265c2a8d
unknown <isc242099@itsatlixco.edu.mx>
2026-04-26 22:57:40 -0600

    coneccion de gastos
    Conexion de gastos falta imagenes

--- sistemaVabel_AppMovil/Views/MainPage.xaml.cs
+++ sistemaVabel_AppMovil/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace sistemaVabel_AppMovil
+﻿using sistemaVabel_AppMovil.Vista;
+
+namespace sistemaVabel_AppMovil
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,12 @@
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        // Este es el método que se ejecutará al presionar el botón rojo
+        private async void OnAgregarGastoClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ModeloGastos());
         }
     }
 }
